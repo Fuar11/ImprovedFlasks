41e5ee29a098113bfaac09bd265d8d908dffae0f
Fuar11 <mcaa6556@gmail.com>
2024-11-30 10:45:41 -0500

    Added flask to food & drink section of inventory

--- Patches/FlaskPatches.cs
+++ Patches/FlaskPatches.cs
@@ -6,13 +6,28 @@ using System.Threading.Tasks;
 using Il2Cpp;
 using HarmonyLib;
 using MelonLoader;
+using Il2CppTLD.Gear;
 
 namespace ImprovedFlasks.Patches
 {
     internal class FlaskPatches
     {
 
+        [HarmonyPatch(typeof(GearItem), nameof(GearItem.Awake))]
 
+        public class AddFlaskToFoodFilter
+        {
+
+            public static void Postfix(GearItem __instance)
+            {
+
+                if (__instance.name.ToLowerInvariant().Contains("flask"))
+                {
+                    __instance.GearItemData.m_Type = GearType.Food | GearType.Tool;
+                }
+            }
+
+        }
 
     }
 }
