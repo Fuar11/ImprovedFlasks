20ec7f90ec4b8ec5dd8c287b451222d59e2574f1
Fuar11 <mcaa6556@gmail.com>
2026-01-06 19:14:52 -0500

    Updated to TLD 2.51

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -21,11 +21,11 @@ namespace ImprovedFlasks
         /// <summary>Who made the mod</summary>
         public const string Author                          = "Fuar";
         /// <summary>Current version (Using Major.Minor.Build) </summary>
-        public const string Version                         = "1.2.2";
+        public const string Version                         = "1.2.3";
         /// <summary>Name used on GUI's, like ModSettings</summary>
         public const string GUIName                         = "Improved Flasks";
         /// <summary>The minimum Melon Loader version that your mod requires</summary>
-        public const string MelonLoaderVersion              = "0.6.6";
+        //public const string MelonLoaderVersion              = "0.6.6";
         #endregion
 
         #region Optional
